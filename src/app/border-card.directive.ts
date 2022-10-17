59cffabe4acff1c581bc01088337b38779867267
julienjego <jego.jj@gmail.com>
2022-10-17 09:52:39 +0200

    Css and border directive

---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,33 @@
+import { Directive, ElementRef, HostListener, Input } from "@angular/core";
+
+@Directive({
+    selector: "[pkmnBorderCard]",
+})
+export class BorderCardDirective {
+    private initialColor = "crimson";
+    private defaultColor = "#009688";
+    private defaultHeight = 300;
+
+    constructor(private el: ElementRef) {
+        this.setHeight(this.defaultHeight);
+        this.setBorder(this.initialColor);
+    }
+
+    @Input("pkmnBorderCard") borderColor!: string;
+
+    @HostListener("mouseenter") onMouseEnter() {
+        this.setBorder(this.borderColor || this.defaultColor);
+    }
+
+    @HostListener("mouseleave") onMouseLeave() {
+        this.setBorder(this.initialColor);
+    }
+
+    private setHeight(height: number) {
+        this.el.nativeElement.style.height = `${height}px`;
+    }
+
+    private setBorder(color: string) {
+        this.el.nativeElement.style.border = `solid 2px ${color}`;
+    }
+}
