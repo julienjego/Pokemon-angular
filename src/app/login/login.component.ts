e8dbdc5f83e5414461eb5a26326866061e4b8850
julienjego <jego.jj@gmail.com>
2022-11-04 10:29:49 +0100

    Last commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,48 @@
+import { AuthService } from "./../auth.service";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+
+@Component({
+    selector: "app-login",
+    templateUrl: "./login.component.html",
+    styles: [],
+})
+export class LoginComponent implements OnInit {
+    message: string = "Vous êtes déco (pika/pika)";
+    name!: string;
+    password!: string;
+    auth!: AuthService;
+
+    constructor(private authService: AuthService, private router: Router) {}
+
+    ngOnInit(): void {
+        this.auth = this.authService;
+    }
+
+    setMessage() {
+        if (this.auth.isLoggedIn) {
+            this.message = "Vous êtes connecté.";
+        } else {
+            this.message = "Id ou mdp incorrect.";
+        }
+    }
+
+    login() {
+        this.message = "Tentative de connexion en cours...";
+        this.auth
+            .login(this.name, this.password)
+            .subscribe((isLoggedIn: boolean) => {
+                this.setMessage();
+                if (isLoggedIn) {
+                    this.router.navigate(["/pokemons"]);
+                } else {
+                    this.password = "";
+                    this.router.navigate(["/login"]);
+                }
+            });
+    }
+    logout() {
+        this.auth.logout();
+        this.message = "Vous êtes déco";
+    }
+}
--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from "./../auth.guard";
 import { PokemonService } from "./pokemon.service";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
@@ -10,14 +11,30 @@ import { PokemonFormComponent } from "./pokemon-form/pokemon-form.component";
 import { FormsModule } from "@angular/forms";
 import { EditPokemonComponent } from "./edit-pokemon/edit-pokemon.component";
 import { AddPokemonComponent } from "./add-pokemon/add-pokemon.component";
-import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
-import { LoaderComponent } from './loader/loader.component';
+import { SearchPokemonComponent } from "./search-pokemon/search-pokemon.component";
+import { LoaderComponent } from "./loader/loader.component";
 
 const pokemonRoutes: Routes = [
-    { path: "edit/pokemon/:id", component: EditPokemonComponent },
-    { path: "pokemon/add", component: AddPokemonComponent },
-    { path: "pokemons", component: ListPokemonComponent },
-    { path: "pokemon/:id", component: DetailPokemonComponent },
+    {
+        path: "edit/pokemon/:id",
+        component: EditPokemonComponent,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: "pokemon/add",
+        component: AddPokemonComponent,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: "pokemons",
+        component: ListPokemonComponent,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: "pokemon/:id",
+        component: DetailPokemonComponent,
+        canActivate: [AuthGuard],
+    },
 ];
 
 @NgModule({
